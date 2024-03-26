561bb52c1882017ab598d1b4b9c1266c9ed499e3
Spook <spook.devv@gmail.com>
2024-03-26 12:05:47 -0400

    Dynamically change the mode of expense management screen (edit, add)

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,16 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId; // The !!someVariable expression is a common JavaScript pattern used to convert a value to its corresponding boolean representation
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return <Text>Manage Expense Screen</Text>;
 }
 
